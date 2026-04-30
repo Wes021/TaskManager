22ac3c424ddbb6166392dde7a4d735d4e0596db9
Wes021 <122152471+Wes021@users.noreply.github.com>
2026-04-30 20:17:07 +0300

    Add Unit of Work interface and implementation
    Introduces the `IIdentityMouduleUoW` interface with a
    `SaveChangesAsync` method for asynchronous saving.
    Implements the `IdentityMouduleUoW` class that uses
    `AppDbContext` to perform save operations.

---
+++ Identity/Identity.Domain/IUnitOfWork/IIdentityMouduleUoW.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Identity.Domain.IUnitOfWork
+{
+    public interface IIdentityMouduleUoW
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Identity/Identity.Infrastructure/UnitOfWork/IdentityMouduleUoW.cs
@@ -0,0 +1,25 @@
+﻿using Identity.Identity.Domain.IUnitOfWork;
+using Module.Identity.Infrastructure.DbSettings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Identity.Infrastructure.UnitOfWork
+{
+    public class IdentityMouduleUoW : IIdentityMouduleUoW
+    {
+        private readonly AppDbContext _context;
+
+        public IdentityMouduleUoW(AppDbContext context)
+        {
+            _context = context;
+        }
+        public Task<int> SaveChangesAsync(
+       CancellationToken cancellationToken = default)
+        {
+            return _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
